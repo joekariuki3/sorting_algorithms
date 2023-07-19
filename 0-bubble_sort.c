c9ab5ba08acf04a791b94b9bca3a11620a04b024
joekariuki3 <joekariuki3@gmail.com>
2023-07-19 21:07:25 +0300

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include"sort.h"
+
+/**
+ * bubble_sort - sorts an array in assending order
+ * @array: intagers to be sorted
+ * @size: number of integers in @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0;
+	int temp = 0, swap = 0;
+
+	if (size > 1 && array != NULL)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			swap = 0;
+			for (j = 0; j < size - 1 - i; j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					swap = 1;
+					print_array(array, size);
+				}
+			}
+
+			if (swap == 0)
+				break;
+		}
+	}
+
+}
